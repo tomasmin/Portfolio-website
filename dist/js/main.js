6aa9082a3ad05c64ee97a2ac092e5eb1101efa7a
tomasmin <tomas.mineika@akka.eu>
2019-11-08 22:38:10 +0100

    finishing touches

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,6 @@
 const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
-const menuBranding = document.querySelector(".menu-branding");
 const navItems = document.querySelectorAll(".nav-item");
 
 let showMenu = false;
@@ -13,7 +12,6 @@ function toggleMenu() {
     menuBtn.classList.add("close");
     menu.classList.add("show");
     menuNav.classList.add("show");
-    menuBranding.classList.add("show");
     navItems.forEach(item => item.classList.add("show"));
 
     showMenu = true;
@@ -21,7 +19,6 @@ function toggleMenu() {
     menuBtn.classList.remove("close");
     menu.classList.remove("show");
     menuNav.classList.remove("show");
-    menuBranding.classList.remove("show");
     navItems.forEach(item => item.classList.remove("show"));
 
     showMenu = false;
